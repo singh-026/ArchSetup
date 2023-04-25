7d489b03d1093128d3764ed7beb219ded229c2f8
Mobcoder ID-226 <mobcoderid-226@Mobcoders-Mac-mini-103.local>
2023-04-25 18:12:00 +0530

    bugs : bugs on project setup

--- src/navigation/Navigation.tsx
+++ src/navigation/Navigation.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {NavigationContainer} from '@react-navigation/native';
 import {RootStackParamList} from '../_types';
+import Dashboard from '../screens/Dashboard';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
-const Navigation = () => {
+const Navigation:()=>JSX.Element = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{headerShown: false}}>
-        {/* <Stack.Screen name="Dashboard" component={Dashboard} /> */}
+        <Stack.Screen name="Dashboard" component={Dashboard} />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ src/screens/Dashboard.tsx
@@ -0,0 +1,13 @@
+import {StyleSheet, Text, View} from 'react-native';
+
+const Dashboard = () => {
+  return (
+    <View>
+      <Text>Dashboard</Text>
+    </View>
+  );
+};
+
+export default Dashboard;
+
+const styles = StyleSheet.create({});
