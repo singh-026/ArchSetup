3c42fa644b23c1bd1b63a7c198b064aeb842e9a6
Mobcoder ID-226 <mobcoderid-226@Mobcoders-Mac-mini-103.local>
2023-04-26 19:20:46 +0530

    .env : done with dotent setup

--- src/screens/Dashboard.tsx
+++ src/screens/Dashboard.tsx
@@ -1,9 +1,11 @@
 import {StyleSheet, Text, View} from 'react-native';
+import {API_KEY} from "@env"
 
 const Dashboard = () => {
+  console.log("-----API_KEY_RESPONSE-----", API_KEY)
   return (
     <View>
-      <Text>Dashboard</Text>
+      <Text>Dashboard,{API_KEY}</Text>
     </View>
   );
 };
